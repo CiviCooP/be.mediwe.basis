fd1faff9d899d5476697081c3f9c220d250f151c
Christophe Deman <christophe.deman@mediwe.be>
2017-07-24 23:02:26 +0200

    Voorbereiding KlantMedewerker

--- CRM/Basis/Form/KlantMedewerker.php
+++ CRM/Basis/Form/KlantMedewerker.php
@@ -11,12 +11,35 @@ class CRM_Basis_Form_KlantMedewerker extends CRM_Core_Form {
 
   public function buildQuickForm() {
 
+    $this->add('text', 'employee_national_nbr', ts('Rijksregisternummer '), array(), FALSE);
+    $this->add('text', 'employee_personnel_nbr', ts('Personeelsnummer'), array(), FALSE);
 
     $this->add('text', 'name', ts('Naam werknemer'), array(), TRUE);
     $this->add('text', 'supplemental_address_1', ts('Tweede lijn'), array(), FALSE);
     $this->add('text', 'street_address', ts('Adres (straat en huisnummer)'), array(), TRUE);
     $this->add('text', 'postal_code', ts('Postcode'), array(), TRUE);
     $this->add('text', 'city', ts('Gemeente'), array(), TRUE);
+
+    $this->add('text', 'employee_partner', ts('Partner'), array(), FALSE);
+
+    $this->add('text', 'phone', ts('Telefoon'), array(), FALSE);
+    $this->add('text', 'mobile', ts('GSM'), array(), FALSE);
+
+    $this->add('text', 'employee_level1', ts('Niveau 1'), array(), FALSE);
+    $this->add('text', 'employee_code_level2', ts('Code Niveau 2'), array(), FALSE);
+    $this->add('text', 'employee_level2', ts('Niveau 2'), array(), FALSE);
+    $this->add('text', 'employee_level3', ts('Niveau 3'), array(), FALSE);
+
+    $this->add('text', 'employee_function', ts('Functie'), array(), FALSE);
+
+    $dateParts     = implode( CRM_Core_DAO::VALUE_SEPARATOR, array( 'Y', 'M' ) );
+
+    $this->add( 'date', 'employee_date_in',
+          ts('Datum in dienst'),
+          CRM_Core_SelectValues::date('custom', 1, 0, $dateParts ) );
+    $this->add( 'date', 'employee_date_out',
+          ts('Datum uit dienst'),
+          CRM_Core_SelectValues::date('custom', 1, 0, $dateParts ) );
 
     $this->addButtons(array(
       array('type' => 'next', 'name' => ts('Save'), 'isDefault' => true,),
@@ -25,48 +48,59 @@ class CRM_Basis_Form_KlantMedewerker extends CRM_Core_Form {
 
     // export form elements
     $this->assign('elementNames', $this->getRenderableElementNames());
+
+    if (isset($this->_contactData[0])) {
+          // set values to screen
+          $this->getElement('employee_national_nbr')->setValue($this->_contactData[0]['employee_national_nbr']);
+          $this->getElement('employee_personnel_nbr')->setValue($this->_contactData[0]['employee_personnel_nbr']);
+          $this->getElement('name')->setValue($this->_contactData[0]['name']);
+          $this->getElement('supplemental_address_1')->setValue($this->_contactData[0]['supplemental_address_1']);
+          $this->getElement('street_address')->setValue($this->_contactData[0]['street_address']);
+          $this->getElement('postal_code')->setValue($this->_contactData[0]['postal_code']);
+          $this->getElement('city')->setValue($this->_contactData[0]['city']);
+
+          $this->getElement('employee_partner')->setValue($this->_contactData[0]['employee_partner']);
+          $this->getElement('phone')->setValue($this->_contactData[0]['phone']);
+          $this->getElement('mobile')->setValue($this->_contactData[0]['mobile']);
+          $this->getElement('employee_level1')->setValue($this->_contactData[0]['employee_level1']);
+          $this->getElement('employee_code_level2')->setValue($this->_contactData[0]['employee_code_level2']);
+          $this->getElement('employee_level2')->setValue($this->_contactData[0]['employee_level2']);
+          $this->getElement('employee_level3')->setValue($this->_contactData[0]['employee_level3']);
+
+          $this->getElement('employee_function')->setValue($this->_contactData[0]['employee_function']);
+          $this->getElement('employee_date_in')->setValue($this->_contactData[0]['employee_date_in']);
+          $this->getElement('employee_date_out')->setValue($this->_contactData[0]['employee_date_out']);
+    }
+
+
     parent::buildQuickForm();
   }
 
   public function preProcess() {
-    $this->setSectorList();
-    $this->setContactData(25);
+
+      $id =   CRM_Utils_Request::retrieve('id', 'Integer');
+      if ($id) {
+          $this->setContactData($id);
+      }
+
   }
 
   public function postProcess() {
     //CRM_Core_Error::debug('submitValues', $this->_submitValues);
     //exit();
-    $this->saveKlant($this->_submitValues);
+    $this->saveKlantMedewerker($this->_submitValues);
     parent::postProcess();
   }
 
-  private function saveKlant($formValues) {
-    civicrm_api3('Klant', 'create', $formValues);
+  private function saveKlantMedewerker($formValues) {
+    civicrm_api3('KlantMedewerker', 'create', $formValues);
   }
 
-  /**
-   * Method to set the list of sectors
-   */
-  private function setSectorList() {
-    try {
-      $optionValues = civicrm_api3('OptionValue', 'get', array(
-        'option_group_id' => 'sector',
-        'is_active' => 1,
-        'options' > array('limit' => 0),
-      ));
-      foreach ($optionValues['values'] as $optionValue) {
-        $this->_sectorList[$optionValue['value']] = $optionValue['label'];
-      }
-    }
-    catch (CiviCRM_API3_Exception $ex) {
-    }
-    asort($this->_sectorList);
-  }
 
   private function setContactData($id) {
-      $klant = new CRM_Basis_Klant();
+      $medewerker = new CRM_Basis_KlantMedewerker();
 
-      $this->_contactData = $klant->get(array ( 'id' => $id, ));
+      $this->_contactData = $medewerker->get(array ( 'id' => $id, ));
 
   }
 
