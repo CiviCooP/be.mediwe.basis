2f58d4080a7f4f7e66863e5ed85646db6a64f6d7
Erik Hommel <hommel@ee-atwork.nl>
2018-02-15 11:18:37 +0100

    civilinted ziektemelding delete

--- api/v3/Ziektemelding/Delete.php
+++ api/v3/Ziektemelding/Delete.php
@@ -1,18 +1,24 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: CDeman
- * Date: 20/07/2017
- * Time: 23:43
-*/
-
-
+ * Ziektemelding.Create API specification (optional)
+ * This is used for documentation and validation.
+ *
+ * @param array $spec description of fields supported by this API call
+ * @return void
+ * @see http://wiki.civicrm.org/confluence/display/CRMDOC/API+Architecture+Standards
+ */
 function _civicrm_api3_ziektemelding_Delete_spec(&$spec) {
-    $spec['id']['api.required'] = 1;
+  $spec['id'] = array(
+    'name' => 'id',
+    'title' => 'case_id',
+    'description' => 'ID dossier ziektemelding',
+    'api.required' => 1,
+    'type' => CRM_Utils_Type::T_INT,
+  );
 }
 
 /**
- * Klant.Create API
+ * Klant.Delete API
  *
  * @param array $params
  * @return array API result descriptor
@@ -20,12 +26,8 @@ function _civicrm_api3_ziektemelding_Delete_spec(&$spec) {
  * @see civicrm_api3_create_error
  * @throws API_Exception
  */
-
 function civicrm_api3_ziektemelding_Delete($params) {
-
-    $ziektemelding = new CRM_Basis_Ziektemelding();
-    $returnValues = $ziektemelding->deleteWithId($params['id']);
-
-    return civicrm_api3_delete_success($returnValues, $params, 'Ziektemelding', 'Delete');
-
+  $ziektemelding = new CRM_Basis_Ziektemelding();
+  $returnValues = $ziektemelding->deleteWithId($params['id']);
+  return civicrm_api3_create_success($returnValues, $params, 'Ziektemelding', 'Delete');
 }
