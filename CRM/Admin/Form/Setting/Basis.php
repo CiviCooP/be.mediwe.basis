a8b4dfffd75cdce7cf964e0df4ecd58b224ffa9d
Erik Hommel <hommel@ee-atwork.nl>
2018-02-23 20:48:15 +0100

    civilint setting basis class

--- CRM/Admin/Form/Setting/Basis.php
+++ CRM/Admin/Form/Setting/Basis.php
@@ -7,20 +7,18 @@
  * @license AGPL-3.0
  */
 
-class CRM_Admin_Form_Setting_Basis extends CRM_Admin_Form_Setting
-{
-    const MEDIWE_PREFERENCES_NAME = 'Mediwe Preferences';
+class CRM_Admin_Form_Setting_Basis extends CRM_Admin_Form_Setting {
+  const MEDIWE_PREFERENCES_NAME = 'Mediwe Preferences';
 
-    protected $_settings =  array(
-        'mediwe_opdrachtemailarts_template_id' => self::MEDIWE_PREFERENCES_NAME,
-        'mediwe_location_type_id' => self::MEDIWE_PREFERENCES_NAME,
-        'mediwe_belgisch_btw_formaat' => self::MEDIWE_PREFERENCES_NAME,
-    );
+  protected $_settings = array(
+    'mediwe_opdrachtemailarts_template_id' => self::MEDIWE_PREFERENCES_NAME,
+    'mediwe_location_type_id' => self::MEDIWE_PREFERENCES_NAME,
+    'mediwe_belgisch_btw_formaat' => self::MEDIWE_PREFERENCES_NAME,
+  );
 
-    public function buildQuickForm()
-    {
-        CRM_Utils_System::setTitle(ts('Settings - Mediwe Basis'));
-        parent::buildQuickForm();
-    }
+  public function buildQuickForm() {
+    CRM_Utils_System::setTitle(ts('Settings - Mediwe Basis'));
+    parent::buildQuickForm();
+  }
 
 }
