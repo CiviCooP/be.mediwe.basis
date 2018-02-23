f846b1b4a862f99425b90c7838759ca319e82442
Erik Hommel <hommel@ee-atwork.nl>
2018-02-23 20:45:09 +0100

    toevoegen btw formaat aan basis settings

--- settings/basis.setting.php
+++ settings/basis.setting.php
@@ -3,7 +3,9 @@
  * Settings voor wat nog ingesteld moet worden
  *
  * @author Klaas Eikelboom (CiviCooP) <klaas.eikelboom@civicoop.org>
- * @date 26 Januari 2017
+ * @author Erik Hommel (CiviCooP) <erik.hommel@civicoop.org>
+ * @author Christophe Deman <christophe.deman@mediwe.be>
+ * @date 26 Januari 2018
  * @license AGPL-3.0
  */
 
@@ -41,5 +43,20 @@ return [
     ],
     'title' => 'Standaard locatie type',
     'description' => 'Locatie type dat standaard binnen Mediwe gebruikt wordt',
+  ],
+  'mediwe_belgisch_btw_formaat' => [
+    'add' => '4.7',
+    'help_text' => 'Standaard Belgisch BTW formaat',
+    'is_domain' => 1,
+    'is_contact' => 0,
+    'group_name' => CRM_Admin_Form_Setting_Basis::MEDIWE_PREFERENCES_NAME,
+    'group' => 'mediwe',
+    'type' => 'String',
+    'quick_form_type' => 'Element',
+    'default' => 'BE 123.4567.890',
+    'html_type' => 'text',
+    'data_type' => 'string',
+    'title' => 'Standaard Belgisch BTW formaat',
+    'description' => 'Standaard BTW formaat dat gebruikt wordt voor Belgische organisaties of BTW nummer die met BE beginnen',
   ],
 ];
